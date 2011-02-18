09695c2d401cc818fb7fac30b60fee51a9848847
George Stagas <gstagas@gmail.com>
2011-02-18 04:28:48 +0200

    First release

---
+++ example.js
@@ -0,0 +1,18 @@
+var connect = require('connect')
+  , imageResizer = require('connect-image-resizer')
+
+var server = connect.createServer(
+  imageResizer(__dirname + '/public', {
+    'sepia': function(gm, source, target, w, h, finish) {
+      gm(source)
+        .resize(w, h)
+        .sepia()
+        .write(target, finish)
+    }
+  })
+, connect.staticProvider(__dirname + '/public')
+)
+
+server.listen(8080, 'localhost')
+
+console.log('Server started on port 8080')
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/connect-image-resizer')
